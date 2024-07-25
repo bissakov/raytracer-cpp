cab483d2bffeb379ca5d99dedae1b6ef0efbaa6e
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-26 00:25:37 +0500

    (chore): Raylib empty window

--- build.bat
+++ build.bat
@@ -4,7 +4,12 @@ call vcvarsall.bat x64 > nul 2>&1
 mkdir build > nul 2>&1
 pushd build
 
-cl -Fe: raytracer.exe -nologo -Oi -GR- -EHa- -MT -Gm- -Od -W4 -WX -wd4201 -wd4127 -wd4100 -FC -Z7 -Fm: lox.map /I.. /Isrc ../src/main.cpp ../src/point_vector.cpp ../src/test_suite.cpp /link -opt:ref
+set files=../src/main.cpp ../src/point_vector.cpp ../src/color.cpp ../src/test_suite.cpp
+set libs=../lib/raylib.lib user32.lib gdi32.lib msvcrt.lib shell32.lib winmm.lib
+set ignore_warnings=-wd4201 -wd4127 -wd4100
+set exe_name=raytracer.exe
+
+cl -Fe: %exe_name% -nologo -Oi -GR- -EHa- -MT -Gm- -Od -W4 -WX %ignore_warnings% -FC -Z7 -Fm: lox.map -I.. -Isrc -I../include %files% %libs% /link -opt:ref /NODEFAULTLIB:libcmt
 
 popd
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,126 +1,23 @@
+#include <raylib/raylib.h>
 #include <src/main.h>
-#include <src/point_vector.h>
-#include <src/test_suite.h>
 
-#include <cmath>
+int main(int argc, char *argv[]) {
+  const int width = 1920;
+  const int height = 1080;
+  const int target_fps = 60;
 
-int main(int argc, char* argv[]) {
-  TestFramework framework;
+  InitWindow(width, height, "Ray tracer");
 
-  framework.AddTest("Point + vector", []() -> bool {
-    Point left = {3.0f, -2.0f, 5.0f};
-    Vector right = {-2.0f, 3.0f, 1.0f};
-    Point expected = {1.0f, 1.0f, 6.0f};
-    return AssertEq(left + right, expected);
-  });
+  SetTargetFPS(target_fps);
 
-  framework.AddTest("Point + vector", []() -> bool {
-    Point left = {3.0f, -2.0f, 5.0f};
-    Vector right = {-2.0f, 3.0f, 1.0f};
-    Point expected = {1.0f, 1.0f, 6.0f};
-    return AssertEq(left + right, expected);
-  });
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("hello world!", 200, 200, 16, LIGHTGRAY);
+    EndDrawing();
+  }
 
-  framework.AddTest("Point - point", []() -> bool {
-    Point left = {3.0f, 2.0f, 1.0f};
-    Point right = {-2.0f, 3.0f, 1.0f};
-    Vector expected = {5.0f, -1.0f, 0.0f};
-    return AssertEq(left - right, expected);
-  });
-
-  framework.AddTest("Point - vector", []() -> bool {
-    Point left = {3.0f, 2.0f, 1.0f};
-    Vector right = {5.0f, 6.0f, 7.0f};
-    Point expected = {-2.0f, -4.0f, -6.0f};
-    return AssertEq(left - right, expected);
-  });
-
-  framework.AddTest("Vector - vector", []() -> bool {
-    Vector left = {3.0f, 2.0f, 1.0f};
-    Vector right = {5.0f, 6.0f, 7.0f};
-    Vector expected = {-2.0f, -4.0f, -6.0f};
-    return AssertEq(left - right, expected);
-  });
-
-  framework.AddTest("Negate vector", []() -> bool {
-    Vector v = {1.0f, -2.0f, 3.0f};
-    Vector expected = {-1.0f, 2.0f, -3.0f};
-    return AssertEq(-v, expected);
-  });
-
-  framework.AddTest("Multiply vector", []() -> bool {
-    Vector v = {1.0f, -2.0f, 3.0f};
-    Vector expected = {3.5f, -7.0f, 10.5f};
-    return AssertEq(v * 3.5f, expected);
-  });
-
-  framework.AddTest("Divide vector by multiplying", []() -> bool {
-    Vector v = {1.0f, -2.0f, 3.0f};
-    Vector expected = {0.5f, -1.0f, 1.5f};
-    return AssertEq(v * 0.5f, expected);
-  });
-
-  framework.AddTest("Divide vector", []() -> bool {
-    Vector v = {1.0f, -2.0f, 3.0f};
-    Vector expected = {0.5f, -1.0f, 1.5f};
-    return AssertEq(v / 2.0f, expected);
-  });
-
-  framework.AddTest("The magnitude of a vector 1", []() -> bool {
-    Vector v = {1.0f, 0.0f, 0.0f};
-    float expected = 1.0f;
-    return AssertFloatEq(v.Magnitude(), expected);
-  });
-
-  framework.AddTest("The magnitude of a vector 2", []() -> bool {
-    Vector v = {0.0f, 1.0f, 0.0f};
-    float expected = 1.0f;
-    return AssertFloatEq(v.Magnitude(), expected);
-  });
-
-  framework.AddTest("The magnitude of a vector 3", []() -> bool {
-    Vector v = {0.0f, 0.0f, 1.0f};
-    float expected = 1.0f;
-    return AssertFloatEq(v.Magnitude(), expected);
-  });
-
-  framework.AddTest("The magnitude of a vector 4", []() -> bool {
-    Vector v = {1.0f, 2.0f, 3.0f};
-    float expected = sqrt(14.0f);
-    return AssertFloatEq(v.Magnitude(), expected);
-  });
-
-  framework.AddTest("The magnitude of a vector 5", []() -> bool {
-    Vector v = {-1.0f, -2.0f, -3.0f};
-    float expected = sqrt(14.0f);
-    return AssertFloatEq(v.Magnitude(), expected);
-  });
-
-  framework.AddTest("Normalize vector 1", []() -> bool {
-    Vector v = {4.0f, 0.0f, 0.0f};
-    Vector expected = {1.0f, 0.0f, 0.0f};
-    return AssertEq(v.Normalize(), expected);
-  });
-
-  framework.AddTest("Normalize vector 2", []() -> bool {
-    Vector v = {1.0f, 2.0f, 3.0f};
-    Vector expected = {1.0f / sqrt(14.0f), 2.0f / sqrt(14.0f),
-                       3.0f / sqrt(14.0f)};
-    return AssertEq(v.Normalize(), expected);
-  });
-
-  framework.AddTest("The magnitude of a normalized vector", []() -> bool {
-    Vector v = {1.0f, 2.0f, 3.0f};
-
-    Vector nv = v.Normalize();
-    float mn = nv.Magnitude();
-
-    float expected = 1.0f;
-
-    return AssertFloatEq(mn, expected);
-  });
-
-  framework.RunTest();
+  CloseWindow();
 
   return 0;
 }
