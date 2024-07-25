d3245d057c48c1f6709ad792654778cc2545641a
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-25 14:12:09 +0500

    (feat): Break down Tuple into Point and Vector. Use operator overloading

--- src/main.h
+++ src/main.h
@@ -1,11 +1,31 @@
 #ifndef SRC_MAIN_H_
 #define SRC_MAIN_H_
 
-struct Tuple {
+typedef struct Point Point;
+typedef struct Vector Vector;
+
+struct Point {
   float x;
   float y;
   float z;
-  float w;
+
+  Point operator+(const Vector& other) const;
+  Point operator-(const Vector& other) const;
+  Vector operator-(const Point& other) const;
+  bool operator==(const Point& other) const;
+  bool operator!=(const Point& other) const;
+};
+
+struct Vector {
+  float x;
+  float y;
+  float z;
+
+  Vector operator+(const Vector& other) const;
+  Vector operator-(const Vector& other) const;
+  Vector operator-() const;
+  bool operator==(const Vector& other) const;
+  bool operator!=(const Vector& other) const;
 };
 
 #endif  // SRC_MAIN_H_
