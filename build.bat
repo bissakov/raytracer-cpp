b43c5aed42313ec721c9b4b4b63dc1de02b435d0
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-25 20:11:43 +0500

    (chore): Update build files

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ call vcvarsall.bat x64 > nul 2>&1
 mkdir build > nul 2>&1
 pushd build
 
-cl -Fe: raytracer.exe -nologo -Oi -GR- -EHa- -MT -Gm- -Od -W4 -WX -wd4201 -wd4127 -wd4100 -FC -Z7 -Fm: lox.map ../src/main.cpp /link -opt:ref
+cl -Fe: raytracer.exe -nologo -Oi -GR- -EHa- -MT -Gm- -Od -W4 -WX -wd4201 -wd4127 -wd4100 -FC -Z7 -Fm: lox.map /I.. /Isrc ../src/main.cpp ../src/point_vector.cpp ../src/test_suite.cpp /link -opt:ref
 
 popd
 
