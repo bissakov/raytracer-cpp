df048cd6eaac14d8849adda4d9c0897358ec3ba9
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-28 00:03:13 +0500

    (chore): Update build script

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ call vcvarsall.bat x64 > nul 2>&1
 mkdir build > nul 2>&1
 pushd build
 
-set files=../src/main.cpp ../src/point_vector.cpp ../src/pixel.cpp ../src/canvas.cpp ../src/test_suite.cpp ../tests/tests.cpp
+set files=../src/main.cpp ../src/point_vector.cpp ../src/pixel.cpp ../src/canvas.cpp ../src/file_io.cpp ../src/test_suite.cpp ../tests/tests.cpp ../tests/test_point_vector.cpp ../tests/test_color.cpp ../tests/test_canvas.cpp
 set libs=../lib/raylib.lib user32.lib gdi32.lib msvcrt.lib shell32.lib winmm.lib
 set ignore_warnings=-wd4201 -wd4127 -wd4100
 set exe_name=raytracer.exe
