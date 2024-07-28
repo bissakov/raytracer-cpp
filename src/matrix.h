d43550b46bf7f526e7afe74588bca4e58330d0e6
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-28 23:59:35 +0500

    (fix): Check the number of input values to not exceed matrix size

--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,8 @@
 #ifndef SRC_MATRIX_H_
 #define SRC_MATRIX_H_
 
+#include <cassert>
+
 struct Matrix {
   int rows;
   int cols;
@@ -12,6 +14,8 @@ struct Matrix {
   template <typename... T>
   void Populate(T... floats) const {
     float values_[] = {floats...};
+    assert(sizeof(values_) == rows * cols * sizeof(float) &&
+           "Invalid number of values");
     int idx = 0;
     for (int row = 0; row < rows; ++row) {
       for (int col = 0; col < cols; ++col) {
