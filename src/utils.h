b599d68769323211b6de14d773f428edbeaba4d5
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-08-04 21:38:35 +0500

    (feat): Map range function

--- src/utils.h
+++ src/utils.h
@@ -13,6 +13,15 @@ constexpr size_t Max(const size_t a, const size_t b) {
   return (a > b) ? a : b;
 }
 
+constexpr size_t Map(size_t value, size_t src_min, size_t src_max,
+                     size_t dest_min, size_t dest_max) {
+  size_t src_range = src_max - src_min;
+  size_t dest_range = dest_max - dest_min;
+  size_t scaled_value = (value - src_min) * dest_range / src_range;
+
+  return dest_min + scaled_value;
+}
+
 constexpr size_t Clamp(const size_t value, const size_t min_value,
                        const size_t max_value) {
   return Max(min_value, Min(value, max_value));
