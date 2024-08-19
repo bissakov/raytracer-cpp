ad8928a37097a20c296e57577f2bbd233f864c70
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-08-19 12:51:15 +0500

    (chore): Commented future linked libraries

--- build.bat
+++ build.bat
@@ -19,6 +19,7 @@ set src_files=%src_dir%\point.cpp %src_dir%\vector.cpp %src_dir%\color.cpp ^
 	%src_dir%\utils.cpp
 set test_files=%tests_dir%\tests.cpp
 
+REM set third_party=User32.lib Gdi32.lib Shell32.lib
 set includes=-I%cwd% -Isrc -I%include_dir% %entry% %src_files% %test_files%
 
 set warnings=-W4 -WX -wd4201 -wd4127 -wd4100 -wd4514 -wd4668 -wd5045
@@ -26,7 +27,7 @@ set exe_name=raytracer.exe
 
 cl -Fe: %exe_name% -std:c++20 -nologo ^
 	-Oid -GR- -EHs -MT -MP -Gm- %warnings% -FC -Zi -Fm: lox.map ^
-	%includes% %libs% /link -opt:ref | gnomon
+	%includes% %third_party% /link -opt:ref | gnomon
 
 if errorlevel 1 del %exe_name% && echo Build failed...
 
